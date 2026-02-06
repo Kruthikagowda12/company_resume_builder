012b129dd5e94b9c720f3975484a2bb59a9bcdc8
Kruthikagowda12 <kruthikakj2002@gmail.com>
2026-02-06 14:34:41 +0530

    Redirect root URL to dashboard

--- generator/urls.py
+++ generator/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views  # <--- THIS IS THE MISSING LINE
 
 urlpatterns = [
-    path('', views.search_view, name='search'),
+    path('', views.dashboard, name='home'),
+    path('search/', views.search_view, name='search'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('delete/<int:emp_id>/', views.delete_employee, name='delete_employee'), # New line
     path('resume/', views.generate_resume, name='generate_resume'), # Updated line
